b8e388a77767e1c1e856db55b36577dbf20a2711
mehedi08cloud <mehedimurad08@gmail.com>
2023-10-20 12:23:13 +0600

    setup two different route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config();
 
@@ -31,27 +31,113 @@ async function run() {
     const productCollection = client.db('productDB').collection('product');
     const brandCollection = client.db('productDB').collection('brand');
     const userCollection = client.db('productDB').collection('user');
+    const newsletterCollection = client.db('productDB').collection('newsletter');
 
     
-
+    //for add products
     app.post('/product', async(req, res) =>{
         const newProduct = req.body;
         const result = await productCollection.insertOne(newProduct);
         res.send(result)
     })
 
+    app.get('/product', async(req, res) =>{
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/product/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.put('/product/:id', async(req, res) =>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert:true}
+        const updatedProduct = req.body;
+        const product = {
+            $set: {
+                name: updatedProduct.name,
+                brand: updatedProduct.brand,
+                type: updatedProduct.type,
+                price: updatedProduct.price,
+                rating: updatedProduct.rating,
+                photo: updatedProduct.photo,
+                description: updatedProduct.description,
+            }
+        }
+        const result = await productCollection.updateOne(filter, product, options)
+        res.send(result)
+    })
+    // for the brand adidas
+    app.get('/product/adidas', async(req,res) =>{
+      const cursor = productCollection.find( {brand : 'Adidas'} );
+      const adidasFashion = await cursor.toArray();
+      res.send(adidasFashion);
+    })
+
+    // for the brand Nike
+    app.get('/product/nike', async(req,res) =>{
+      const cursor = productCollection.find( {brand : 'Nike'} );
+      const nikeFashion = await cursor.toArray();
+      res.send(nikeFashion);
+    })
+
+    // for the brand Puma
+    app.get('/product/puma', async(req,res) =>{
+      const cursor = productCollection.find( {brand : 'Puma'} );
+      const pumaFashion = await cursor.toArray();
+      res.send(pumaFashion);
+    })
+
+    // for the brand Levis
+    app.get('/product/levis', async(req,res) =>{
+      const cursor = productCollection.find( {brand : 'Levis'} );
+      const levisFashion = await cursor.toArray();
+      res.send(levisFashion);
+    })
+
+    // for the brand Gucci
+    app.get('/product/gucci', async(req,res) =>{
+      const cursor = productCollection.find( {brand : 'Gucci'} );
+      const gucciFashion = await cursor.toArray();
+      res.send(gucciFashion);
+    })
+    // for the brand Zara
+    app.get('/product/zara', async(req,res) =>{
+      const cursor = productCollection.find( {brand : 'Zara'} );
+      const zaraFashion = await cursor.toArray();
+      res.send(zaraFashion);
+    })
+
+
+    
+
+    //for brand
     app.get('/brand', async(req,res) =>{
         const cursor =brandCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+
     app.post('/brand', async(req, res) => {
         const newBrand = req.body;
         const result = await brandCollection.insertOne(newBrand);
         res.send(result)
     })
 
+    
     //user APIs
+
+    app.post('/newsletter', async(req, res) =>{
+      const subscriber = req.body;
+      const result =await newsletterCollection.insertOne(subscriber);
+      res.send(result)
+    })
 
     app.post('/user', async(req, res) =>{
       const newUser = req.body;

55b1828cdc8d45c12eff9f05542822d7cc5c3923
mehedi08cloud <mehedimurad08@gmail.com>
2023-10-20 14:08:43 +0600

    updated few items

--- index.js
+++ index.js
@@ -47,12 +47,14 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/product/:id', async(req, res) => {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await productCollection.findOne(query);
-      res.send(result)
-    })
+    
+
+    // app.get('/product/:id', async(req, res) => {
+    //   const id = req.params.id;
+    //   const query = {_id: new ObjectId(id)}
+    //   const result = await productCollection.findOne(query);
+    //   res.send(result)
+    // })
 
     app.put('/product/:id', async(req, res) =>{
         const id = req.params.id;
